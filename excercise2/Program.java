e5db277b26ef77bd9034e787b75abcca235e15d6
Kenny <kenpfowler@gmail.com>
2021-05-31 18:27:35 -0400

    added a driver class

--- excercise2/Program.java
+++ excercise2/Program.java
@@ -1,9 +1,12 @@
 package excercise2;
 import java.util.Arrays;
+import javax.swing.JOptionPane;
 
 public class Program {
 
     public static void main(String[] args) {
+        JOptionPane Lottery = new JOptionPane();
+    String entry = JOptionPane.showInputDialog("Enter a number between...");
     Lotto test = new Lotto();
     System.out.println(Arrays.toString(test.RandomNumbers()));
     }
