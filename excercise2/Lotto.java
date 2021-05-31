2b417b3ad5e2aa8423b07e0267807471b025390c
Kenny <kenpfowler@gmail.com>
2021-05-31 15:41:07 -0400

    created boilerplate for each excercise

--- excercise2/Lotto.java
+++ excercise2/Lotto.java
@@ -1,4 +1,41 @@
 package excercise2;
+import java.util.Random;
+/*
 
+Exercise 2
+
+Design a Lotto class with one array instance variable to hold three random integer values (from 1 to 9).
+Include a constructor that randomly populates the array for a lotto object.
+Also, include a method in the class to return the array.
+Use this class to simulate a simple lotto game in which the user chooses a number between 3 and 27.
+The user runs the lotto up to 5 times and each time the sum of lotto numbers is calculated.
+If the number chosen by the user matches the sum, the user wins and the game ends.
+If the number does not match the sum within five rolls, the computer wins.
+Use methods of JOptionPane class to interact with the user. (3 marks)
+
+*/
+
+//create Lotto class
 public class Lotto {
+
+//initialize an array of to hold random integers
+private int[] _numbers = {0, 0, 0};
+
+//create constructor to populate objects _number field with random integers
+public Lotto()
+{
+    //create object to create random numbers
+    Random randomizer = new Random();
+    for (int number: this._numbers) {
+        //reassign each variable in _numbers array to random int between 1 & 9
+        number = randomizer.nextInt(8) + 1;
+    }
+}
+
+//create method to return array of random numbers
+public int[] Numbers()
+{
+    return _numbers;
+}
+
 }
---
+++ excercise3/Player.java
@@ -0,0 +1,15 @@
+package excercise3;
+
+/*
+
+Exercise 3
+
+Define a Java class “Player” that implements a set of three overloaded static methods.
+The methods should have different set of parameters and perform similar functionalities.
+Call the methods within main method and display the results. (2 marks)
+
+*/
+
+// create player class
+public class Player {
+}
